f218356adda627a29baf29196d0e615ad9d6254a
ashhab <elashhab_fcih@yahoo.com>
2019-09-30 03:30:42 +0200

    fix shopping cart error

--- resources/views/shop/shoppingCart.blade.php
+++ resources/views/shop/shoppingCart.blade.php
@@ -36,49 +36,57 @@
 
     <div class="jumbotron">
         <h1 class="display-3">Shopping Cart</h1>
-        <div class="row">
+        @if ($cart)
             
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <h3>product</h3>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <h3>#</h3>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <h3>Total Price</h3>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                <h3>Action</h3>
-            </div>  
-            
-
-            @foreach ($cart->items as $item)  
-                <div class="row">
-                    <div class="col-md-12">
-                        <hr>
-                    </div>
+        
+            <div class="row">
+                
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <h3>product</h3>
                 </div>
                 <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                    <h3>{{$item['item']['title']}}</h3>
+                    <h3>#</h3>
                 </div>
                 <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                    <h3>{{$item['qty']}}</h3>
+                    <h3>Total Price</h3>
                 </div>
                 <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                    <h3>{{$item['price']}}</h3>
-                </div>
-                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                    <div>
-                        <input class="form-control-sm"  id="num-{{$item['item']['id']}}" onKeyDown="return false" type="number" value="{{$item['qty']}}" data-decimals="4" min="1" max="{{$item['qty']}}" "/>
-                        <button id="remove" data-id="{{$item['item']['id']}}" class="btn btn-success btn-xs js-remove">Remove</button>
-                    </div>
+                    <h3>Action</h3>
                 </div>  
-            @endforeach
-            
+                
 
-        </div>
+                @foreach ($cart->items as $item)  
+                    <div class="row">
+                        <div class="col-md-12">
+                            <hr>
+                        </div>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                        <h3>{{$item['item']['title']}}</h3>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                        <h3>{{$item['qty']}}</h3>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                        <h3>{{$item['price']}}</h3>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                        <div>
+                            <input class="form-control-sm"  id="num-{{$item['item']['id']}}" onKeyDown="return false" type="number" value="{{$item['qty']}}" data-decimals="4" min="1" max="{{$item['qty']}}" "/>
+                            <button id="remove" data-id="{{$item['item']['id']}}" class="btn btn-success btn-xs js-remove">Remove</button>
+                        </div>
+                    </div>  
+                @endforeach
+                
 
-        <a href="/checkout"><button class="btn btn-success">Checkout</button></a>
+            </div>
+            <a href="/checkout"><button class="btn btn-success">Checkout</button></a>
+        @else
+            <div><p>Cart is empty</p></div>
+
+        @endif
+
+        
     </div>
 
 @endsection
